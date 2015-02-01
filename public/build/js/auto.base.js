ab869ffe427f9549fc773f1919f5d53703b29e9c
Tim Erwin <tim.erwin@gmail.com>
2015-02-01 14:39:22 -0500

    0.3.5

---
+++ public/build/js/auto.base.js
@@ -0,0 +1,20 @@
+(function($) {
+  $.event.special.destroyed = {
+    remove: function(o) {
+      if (o.handler) {
+        o.handler();
+      }
+    }
+  };
+})(jQuery);
+
+(function($) {
+  $.fn.center = function() {
+    this.css("position", "absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
+      $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
+      $(window).scrollLeft()) + "px");
+    return this;
+  };
+})(jQuery);
---
+++ public/build/js/auto.dropdown.js
@@ -0,0 +1,51 @@
+var auto = auto || {};
+
+(function(auto, $) {
+
+  var hideAll = function() {
+    $(".dropdown ul:visible").hide();
+  };
+
+  var init = function(){
+    $("body").on('click touchstart', function() {
+      hideAll();
+    });
+  };
+
+  $(document).on('ready', function(){
+    init();
+  });
+
+  var cancelEvent = function(e) {
+    var event = window.event || e;
+
+    if (event.stopPropagation)
+      event.stopPropagation();
+    else
+      event.cancelBubble = true;
+  };
+
+  $.fn.autodropdown = function(o) {
+    return this.each(function() {
+
+      var activator = $(this).children().not("ul").first();
+      var menu = $(this).find("ul:first");
+
+      menu.on('touchstart', cancelEvent);
+
+      activator.on('touchstart', cancelEvent);
+      activator.on('click', function(e) {
+
+        cancelEvent(e);
+
+        if ($(this).parent().find('ul').is(':visible')) {
+          hideAll();
+        } else {
+          hideAll();
+          menu.toggle();
+        }
+      });
+    });
+  };
+
+}(auto || {}, jQuery));
